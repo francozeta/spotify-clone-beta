9fae7979d613257c7678bb12bdf0ee44877279a0
francozeta <perufra@hotmail.com>
2024-05-07 09:54:20 -0500

    Add Layout

---
+++ components/Box.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+
+
+export const Box: FC<BoxProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={twMerge(`
+        bg-neutral-900
+        rounded-lg
+        h-fit
+        w-full
+      `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
